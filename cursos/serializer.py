f819826e73875344d29ee22538cf522edf60fe30
Douglas Tinoco <douglas.bart@hotmail.com>
2023-09-12 13:10:06 -0300

    Validação de dados

--- cursos/serializer.py
+++ cursos/serializer.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from cursos.models import Curso, Avaliacao
-
+from django.db.models import Avg
 
 class AvaliacaoSerializer(serializers.ModelSerializer):
+
+    
+
     class Meta:
         extra_kargs = {
             'email': {'write_only': True}
@@ -10,6 +13,13 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
 
         model = Avaliacao
         fields = ('id', 'curso', 'nome', 'email', 'comentario', 'avaliacao', 'criacao', 'ativo')
+
+
+
+    def validate_avaliacao(self, valor):
+        if valor in range(1, 6):
+            return valor
+        raise serializers.ValidationError("A avaliação precisa ser de 1 a 5 apenas")    
 
 
 
@@ -24,7 +34,15 @@ class CursoSerializer(serializers.ModelSerializer):
 
     #ID
     avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    media_avaliacoes = serializers.SerializerMethodField()
 
     class Meta:
         model = Curso
-        fields = ('id', 'titulo', 'url', 'criacao', 'ativo', 'avaliacoes')    
+        fields = ('id', 'titulo', 'url', 'criacao', 'ativo', 'avaliacoes', 'media_avaliacoes')    
+
+    def get_media_avaliacoes(self, obj):
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')  
+
+        if media is None:
+            return 0
+        return round(media * 2) / 2    
